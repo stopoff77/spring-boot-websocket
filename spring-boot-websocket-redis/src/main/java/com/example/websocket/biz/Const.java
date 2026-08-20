c463fdb6133d56a39d8f0fd57cdcc4755794b805
ds.jang <ds.jang>
2026-08-20 22:07:29 +0900

    no message

--- spring-boot-websocket-redis/src/main/java/com/example/websocket/biz/Const.java
+++ spring-boot-websocket-redis/src/main/java/com/example/websocket/biz/Const.java
@@ -5,4 +5,6 @@ public final class Const {
     public final static String APP = "APP";
     public final static String TM  = "TM";
 
+    public final static String PUBLISH_PREFIX   = "/pub";
+    public final static String SUBSCRIBE_PREFIX = "/sub";
 }
